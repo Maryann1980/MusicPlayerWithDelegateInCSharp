de9a842efa303ae6dfa0ee1268086787f18cbaf0
USER <USER@DESKTOP-GSI7M8Q>
2024-03-18 21:42:20 +0100

    Add project files.

---
+++ MusicPlayerWithDelegateInCSharp/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace MusicPlayerWithDelegateInCSharp
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            MusicPlayerDelegate player = new MusicPlayerDelegate();
+
+            // Subscribe to the events
+            player.SongAdded += HandleSongAdded;
+            player.SongDeleted += HandleSongDeleted;
+
+            player.Run();
+        }
+
+        // Event handler methods
+        static void HandleSongAdded(string message)
+        {
+            Console.WriteLine($"Event: {message}");
+        }
+
+        static void HandleSongDeleted(string message)
+        {
+            Console.WriteLine($"Event: {message}");
+        }
+    }
+}
